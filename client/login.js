ee20087968d88b5afdeae8cf304239ca3c841e06
sankalp <sankalppandey696@gmail.com>
2023-06-29 16:25:13 +0530

    updated login.js with correct endpoints

--- client/login.js
+++ client/login.js
@@ -9,38 +9,41 @@ loginbtn.addEventListener("click",()=>{
       // Now, check whether the password is matched with value fetched from mysql, if yes then check id and according to id, show the table.
 
       
-      fetch("http://localhost:5000/getAll")
+      fetch(
+        "https://juhosiinternshipproject-sankalppyfever.onrender.com/getAll"
+      )
         .then((response) => response.json())
         .then((data) => {
           console.log(data["data"]);
           console.log(data.data[0].Password);
-          if(password == data.data[1].Password){
-              console.log("logged in as Customer1!");
-              const enterCustomerData = document.getElementById("custData");
-              if(enterCustomerData.style.display=="none"){
-                enterCustomerData.style.display = "block";
-              }
-              else{
-                enterCustomerData.style.display= "none";
-              }
-              const submit = document.getElementById("submitBtn");
-              submit.addEventListener("click", () => {
-                const allvalues = document.getElementsByClassName("text");
-                const weight = allvalues[2].value;
-                const boxcount = allvalues[3].value;
-                const quantity = allvalues[4].value;
-                const owner = allvalues[5].value;
-                const item = allvalues[6].value;
-                const date = allvalues[7].value;
-                const company = allvalues[8].value;
-                const shipmentrequest = allvalues[9].value;
-                const trackingID = allvalues[10].value;
-                const shipmentSize = allvalues[11].value;
-                const specification = allvalues[12].value;
-                const checklistQuantity = allvalues[13].value;
-                // console.log(weight,boxcount,quantity,owner);
+          if (password == data.data[1].Password) {
+            console.log("logged in as Customer1!");
+            const enterCustomerData = document.getElementById("custData");
+            if (enterCustomerData.style.display == "none") {
+              enterCustomerData.style.display = "block";
+            } else {
+              enterCustomerData.style.display = "none";
+            }
+            const submit = document.getElementById("submitBtn");
+            submit.addEventListener("click", () => {
+              const allvalues = document.getElementsByClassName("text");
+              const weight = allvalues[2].value;
+              const boxcount = allvalues[3].value;
+              const quantity = allvalues[4].value;
+              const owner = allvalues[5].value;
+              const item = allvalues[6].value;
+              const date = allvalues[7].value;
+              const company = allvalues[8].value;
+              const shipmentrequest = allvalues[9].value;
+              const trackingID = allvalues[10].value;
+              const shipmentSize = allvalues[11].value;
+              const specification = allvalues[12].value;
+              const checklistQuantity = allvalues[13].value;
+              // console.log(weight,boxcount,quantity,owner);
 
-                fetch("http://localhost:5000/customer1", {
+              fetch(
+                "https://juhosiinternshipproject-sankalppyfever.onrender.com/customer1",
+                {
                   headers: {
                     "Content-type": "application/json",
                   },
@@ -59,40 +62,41 @@ loginbtn.addEventListener("click",()=>{
                     specification: specification,
                     checklistQuantity: checklistQuantity,
                   }),
-                })
-                  .then((response) => response.json())
-                  .then(()=>{
-                    alert("data writing completed!");
-                  })
-              });
-              
+                }
+              )
+                .then((response) => response.json())
+                .then(() => {
+                  alert("data writing completed!");
+                });
+            });
           }
-          if(password == data.data[2].Password){
-              console.log("logged in as Customer2!");
-              const enterCustomerData = document.getElementById("custData");
-              if (enterCustomerData.style.display == "none") {
-                enterCustomerData.style.display = "block";
-              } else {
-                enterCustomerData.style.display = "none";
-              }
-              const submit = document.getElementById("submitBtn");
-              submit.addEventListener("click", () => {
-                const allvalues = document.getElementsByClassName("text");
-                const weight = allvalues[2].value;
-                const boxcount = allvalues[3].value;
-                const quantity = allvalues[4].value;
-                const owner = allvalues[5].value;
-                const item = allvalues[6].value;
-                const date = allvalues[7].value;
-                const company = allvalues[8].value;
-                const shipmentrequest = allvalues[9].value;
-                const trackingID = allvalues[10].value;
-                const shipmentSize = allvalues[11].value;
-                const specification = allvalues[12].value;
-                const checklistQuantity = allvalues[13].value;
-                
+          if (password == data.data[2].Password) {
+            console.log("logged in as Customer2!");
+            const enterCustomerData = document.getElementById("custData");
+            if (enterCustomerData.style.display == "none") {
+              enterCustomerData.style.display = "block";
+            } else {
+              enterCustomerData.style.display = "none";
+            }
+            const submit = document.getElementById("submitBtn");
+            submit.addEventListener("click", () => {
+              const allvalues = document.getElementsByClassName("text");
+              const weight = allvalues[2].value;
+              const boxcount = allvalues[3].value;
+              const quantity = allvalues[4].value;
+              const owner = allvalues[5].value;
+              const item = allvalues[6].value;
+              const date = allvalues[7].value;
+              const company = allvalues[8].value;
+              const shipmentrequest = allvalues[9].value;
+              const trackingID = allvalues[10].value;
+              const shipmentSize = allvalues[11].value;
+              const specification = allvalues[12].value;
+              const checklistQuantity = allvalues[13].value;
 
-                fetch("http://localhost:5000/customer2", {
+              fetch(
+                "https://juhosiinternshipproject-sankalppyfever.onrender.com/customer2",
+                {
                   headers: {
                     "Content-type": "application/json",
                   },
@@ -111,67 +115,68 @@ loginbtn.addEventListener("click",()=>{
                     specification: specification,
                     checklistQuantity: checklistQuantity,
                   }),
-                })
-                  .then((response) => response.json())
-                  .then(()=>{
-                    alert("data writing completed!")
-                  })
-              });
-              
+                }
+              )
+                .then((response) => response.json())
+                .then(() => {
+                  alert("data writing completed!");
+                });
+            });
           }
 
-          if(password == data.data[0].Password){
-              console.log("logged in as Admin!");
-              fetch("http://localhost:5000/admin")
-              .then((response)=> response.json())
-              .then(data=>{
+          if (password == data.data[0].Password) {
+            console.log("logged in as Admin!");
+            fetch(
+              "https://juhosiinternshipproject-sankalppyfever.onrender.com/admin"
+            )
+              .then((response) => response.json())
+              .then((data) => {
                 console.log(data);
-                let weight=0;
-                let boxcount=0;
-                let quantity=0;
-                data.data.forEach(obj=>{
-                  weight+= obj.weight;
-                  boxcount+= obj.boxcount;
-                  quantity+= obj.quantity;
+                let weight = 0;
+                let boxcount = 0;
+                let quantity = 0;
+                data.data.forEach((obj) => {
+                  weight += obj.weight;
+                  boxcount += obj.boxcount;
+                  quantity += obj.quantity;
                 });
                 console.log(weight); //insert this in the table.
                 console.log(boxcount); //insert this in the table.
                 console.log(quantity); //insert this in the table.
 
                 const trElementQuantity = document.querySelector(".quantity");
-                const tdElementsQuantity = trElementQuantity.querySelectorAll(".td");
-                tdElementsQuantity.forEach((td ,index)=>{
-                  if(index==3){
-                    td.textContent= quantity;
+                const tdElementsQuantity =
+                  trElementQuantity.querySelectorAll(".td");
+                tdElementsQuantity.forEach((td, index) => {
+                  if (index == 3) {
+                    td.textContent = quantity;
                   }
-                })
+                });
                 const trElementweight = document.querySelector(".weight");
-                const tdElementsweight = trElementweight.querySelectorAll(".td");
-                tdElementsweight.forEach((td ,index)=>{
-                  if(index==3){
-                    td.textContent= weight;
+                const tdElementsweight =
+                  trElementweight.querySelectorAll(".td");
+                tdElementsweight.forEach((td, index) => {
+                  if (index == 3) {
+                    td.textContent = weight;
                   }
-                })
+                });
                 const trElementboxcount = document.querySelector(".boxcount");
-                const tdElementsboxcount = trElementboxcount.querySelectorAll(".td");
-                tdElementsboxcount.forEach((td ,index)=>{
-                  if(index==3){
-                    td.textContent= boxcount;
+                const tdElementsboxcount =
+                  trElementboxcount.querySelectorAll(".td");
+                tdElementsboxcount.forEach((td, index) => {
+                  if (index == 3) {
+                    td.textContent = boxcount;
                   }
-                })
+                });
 
                 const table = document.getElementById("table");
-                if(table.style.display=="none"){
-                  table.style.display= "block";
+                if (table.style.display == "none") {
+                  table.style.display = "block";
                   table.style.backgroundColor = "red";
+                } else {
+                  table.style.display = "none";
                 }
-                else{
-                  table.style.display= "none";
-                }
-
-
-
-              })
+              });
           }
         })
         .catch((error) => console.log(error));
