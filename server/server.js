b83b721c90277c54333ce04d19d89b0b1dac5bb9
sankalp <sankalppandey696@gmail.com>
2023-06-29 11:25:30 +0530

    updated server.js

--- server/server.js
+++ server/server.js
@@ -108,7 +108,7 @@ app.get("/admin", (request,response)=>{
 
 app.get("/", (request, response) => {
   fs.readFile(
-    "C:Users/sanka/OneDrive/Desktop/JuhosiInternship/clientlogin.html",
+    "C:/Users/sanka/OneDrive/Desktop/JuhosiInternship/client/login.html",
     "utf8",
     (err, data) => {
       if (err) {
